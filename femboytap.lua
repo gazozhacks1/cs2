635b31268d10fc710735f5887f4d64b57dbc5e15
gazozhacks1 <gazozhacks.fs@gmail.com>
2026-07-15 21:00:05 +0300

    Update BASE URL for femboytap.lua

--- femboytap.lua
+++ femboytap.lua
@@ -1,4 +1,4 @@
-local BASE        = rawget(_G, "FEMBOY_BASE") or "https://raw.githubusercontent.com/cachorropacoca/aw_cs2v6_femboytap/main/"
+local BASE        = rawget(_G, "FEMBOY_BASE") or "https://raw.githubusercontent.com/gazozhacks1/cs2/main/"
 local GUILIB_URL  = BASE .. "femboytap_guilib.lua"
 local CHANGER_URL = BASE .. "femboytap_changer.lua"
 
